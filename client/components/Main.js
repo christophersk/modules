418de52e821a4a875fdeed32c286570de71ed536
Chris Skene <cnskene@gmail.com>
2017-12-21 17:45:34 -0500

    work done

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,28 +1,67 @@
 import React from 'react';
-import { CSSTransitionGroup } from 'react-transition-group';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 
-import Widget from './Widget';
-import Widget2x from './Widget2x';
+import Widget from './widgets/Widget';
 
-const Main = () => {
-  const styles = {
-    container: {
-      display: 'flex',
-      justifyContent: 'flex-start',
-      flexWrap: 'wrap',
-      alignItems: 'flex-start',
+import FadeAndSlideTransition from './transitions/FadeAndSlide';
+import Fade from './transitions/Fade';
+import SlideFromLeft from './transitions/SlideFromLeft';
+import SlideFromTop from './transitions/SlideFromTop';
+import SlideFromRight from './transitions/SlideFromRight';
+import SlideFromBottom from './transitions/SlideFromBottom';
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    margin: 'auto',
+    width: 1260,
+  }
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      in: true,
     }
+
+    this.toggleEnterState = this.toggleEnterState.bind(this);
   }
 
-  return (
-    <div style={styles.container}>
+  toggleEnterState() {
+    this.state.in ? this.setState({ in: false }) : this.setState({ in: true })
+  }
 
-      <Widget />
-      <Widget2x />
-      <Widget />
-      <Widget />
-    </div>
-  )
+  render() {
+    return (
+      <div className="container-fluid" style={{padding:30}}>
+      <TransitionGroup className="row">
+        <SlideFromLeft propClasses={'col-sm-3'} propStyles={{height: 300}}>
+              <Widget />
+        </SlideFromLeft>
+        <SlideFromTop propClasses={'col-sm-6'} propStyles={{height: 300}}>
+            <Widget />
+        </SlideFromTop>
+        <SlideFromRight propClasses={'col-sm-3'} propStyles={{height: 300}}>
+            <Widget />
+        </SlideFromRight>
+        <SlideFromLeft propClasses={'col-sm-3'} propStyles={{height: 600}}>
+            <Widget />
+        </SlideFromLeft>
+        <SlideFromBottom propClasses={'col-sm-6'} propStyles={{height: 600}}>
+            <Widget />
+        </SlideFromBottom>
+        <SlideFromRight propClasses={'col-sm-3'} propStyles={{height: 600}}>
+            <Widget />
+        </SlideFromRight>
+      </TransitionGroup>
+      </div>
+    )
+  }
 }
 
 export default Main;
+
